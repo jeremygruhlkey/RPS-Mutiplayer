06f2e5e74d04225d9879f8118fbad8347492813e
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-01-11 18:13:05 -0800

    Corrected sync issue from previous commit
    both names update in real time to dom and databse
    need to save variables as objects globally
    then set to data on click
    and update through onValue function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,10 +14,10 @@ var database = firebase.database();
 
 var choices = [ "Rock", "Paper", "Scissors", "Lizard", "Spock"];
 
-var playerOneName;
-var playerTwoName;
+var playerOneName = "";
+var playerTwoName = "";
 
-var playerOneWins = 1;
+var playerOneWins = 0;
 var playerOneLosses = 0;
 
 var playerTwoWins = 0;
@@ -29,9 +29,11 @@ var playerTwoChoice;
 database.ref().on("value", function(snapshot) {
     if (snapshot.child("playerOne").exists()) {
         $(".player-one-name").text(snapshot.val().playerOne);
+        playerOneName = snapshot.val().playerOne;
     }
     if (snapshot.child("playerTwo").exists()) {
         $(".player-two-name").text(snapshot.val().playerTwo);
+        playerTwoName = snapshot.val().playerTwo;
     }
 
     else {
@@ -49,23 +51,27 @@ $("#add-player").on("click", function() {
 
     if (!playerOneName) {
         $(".player-one-name").text(name);
-        // $(".p-one-wins-losses").text("Wins " + playerOneWins + ", Losses " + playerOneLosses);
+        $(".p-one-wins-losses").text("Wins " + playerOneWins + ", Losses " + playerOneLosses);
         playerOneName = name;
+        // oneWins = ref.child("playerOneName/" + playerOneWins)
         
         console.log("player one: " + playerOneName);
           
     }
     else {
         $(".player-two-name").text(name);
+        $(".p-two-wins-losses").text("Wins " + playerTwoWins + ", Losses " + playerTwoLosses); 
         playerTwoName = name;
         
         console.log("player two: " + playerTwoName);
         
     }
-    
-    database.ref().set({
+
+    database.ref().update({
         playerOne: playerOneName,
         playerTwo: playerTwoName
+
+
       });
 
     });
